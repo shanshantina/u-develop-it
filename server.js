f92b6254e1040c04cfc49130caf281f063625ca8
Shanshan Xu <tinaxu84@gmail.com>
2021-02-15 17:35:01 -0500

    add

--- server.js
+++ server.js
@@ -18,38 +18,38 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 
 // GET a single candidate
-// app.get('/api/candidate/:id', (req, res) => {
-//     const sql = `SELECT * FROM candidates
-//                  WHERE id =?`;
-//     const params = [req.params.id];
-//     db.get(sql, params, (err, row) => {
-//         if(err) {
-//             res.status(400).json({error: err.message});
-//             return;
-//         }
-//         res.json({
-//             message: 'success',
-//             data: row
-//         });
-//     }); 
-// });
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+                 WHERE id =?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    }); 
+});
 
 
 // Delete a candidate
-// app.delete('/api/candidate/:id', (req, res) => {
-//     const sql = `DELETE FROM candidates WHERE id = ?`;
-//     const params = [req.params.id];
-//     db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-//         if (err) {
-//           res.status(400).json({error: err.message});
-//           return;
-//         }
-//         res.json({
-//             message: 'successfully deleted',
-//             changes: this.changes
-//         });
-//     });
-// });
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+        if (err) {
+          res.status(400).json({error: err.message});
+          return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
 
 
 // create a candidate
@@ -81,20 +81,20 @@ app.post('/api/candidate', ({body}, res) => {
 
 //all()method to run the SQL query and executes the callback with all the resulting rows that match the query
 // GET all candidates
-// app.get('/api/candidates', (req, res) => {
-//     const sql = `SELECT * FROM candidates`;
-//     const params = [];
-//     db.all(sql, params, (err, rows) => {
-//         if (err) {
-//             res.status(500).json({error: err.message});
-//             return;
-//         }
-//         res.json({
-//             message: 'success',
-//             data: rows
-//         });
-//     });
-// });
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 
 // default response for any other request(Not Found) catch all
